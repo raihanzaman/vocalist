e3052a7ae8aefde8390ea4d59fe6806bf88e5073
Raihan Zaman <raihanzaman.business@gmail.com>
2025-02-13 10:22:29 -0500

    temporary code

---
+++ vocalist.py
@@ -0,0 +1,24 @@
+'''
+import speech_recognition as sr
+
+def recognize_speech():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+    
+    try:
+        print("Recognizing...")
+        text = recognizer.recognize_google(audio)
+        print("You said: " + text)
+        return text
+    except sr.UnknownValueError:
+        print("Could not understand the audio")
+    except sr.RequestError:
+        print("Error connecting to the speech recognition service")
+
+if __name__ == "__main__":
+    recognize_speech()
+'''
+
